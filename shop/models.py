ac2039196c023c362c63c1c4db1f816e59d669b9
walid zakaria adly <walidpiano@yahoo.com>
2018-04-25 15:37:47 +0200

    build models

---
+++ shop/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    class Meta:
+        ordering = ('name', )
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ('name', )
+
+
